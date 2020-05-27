21c8f887681a1a61aaeae4d05458daa81356ab9e
AviAvinav <54372449+AviAvinav@users.noreply.github.com>
2020-05-28 04:15:32 +0530

    Update and rename InstaBotMain.py to InstaLoginBotMain.py

--- InstaLoginBotMain.py
+++ InstaLoginBotMain.py
@@ -2,14 +2,13 @@ from selenium import webdriver
 from time import sleep
 
 
-#chromedriver = "C:\\Users\\HARENDRA\\Downloads\\chromedriver.exe"
-#chrome_options = webdriver.ChromeOptions()
-#chrome_options.add_experimental_option("excludeSwitches", ['enable-automation'])
+chromedriver = "C:\\Users\\USER\\Downloads\\chromedriver.exe"    #you can change the location according to your computer
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("excludeSwitches", ['enable-automation'])
 
 class InstaBot:
     def __init__(self, username, pwd):
-        #self.driver = webdriver.Chrome(chromedriver, options=chrome_options)
-        self.driver = webdriver.Firefox()
+        self.driver = webdriver.Chrome(chromedriver, options=chrome_options)
         self.driver.get("https://instagram.com")
         sleep(2)
         self.driver.find_element_by_xpath("//input[@name=\"username\"]")\
@@ -22,9 +21,9 @@ class InstaBot:
             .click()
         sleep(2)
         self.driver.find_element_by_xpath("//button[contains(text(), 'Not Now')]")\
-            .clear()
-        sleep(120)
+            .click()
+        
 
 
 
-InstaBot('theconcealedgenius@gmail.com', 'aviaviavi1212')
+InstaBot('Username', 'Password')       #change it with your own username and password
